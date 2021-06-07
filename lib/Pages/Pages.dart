5b0fc035c3acfa7a9ef974443986bb9fc3b57cbd
Bryan <Bulatzi@gmail.com>
2021-06-07 14:16:35 -0500

    Default flutter project

---
+++ lib/Pages/Pages.dart
@@ -0,0 +1,3 @@
+//If you create a page, include it here.
+
+export 'package:flutter_project/Pages/WelcomePage.dart';
---
+++ lib/Pages/WelcomePage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+
+      ),
+    );
+  }
+}
