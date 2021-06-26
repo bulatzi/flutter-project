71c976e14deaa1d35297df86ab485a06e580151b
Gabriela Garza <gabygarza404@gmail.com>
2021-06-25 21:32:50 -0500

    Create BottomBar.dart

---
+++ lib/Pages/BottomBar.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_project/Pages/Pages.dart';
+//================Bottom BAr=====================================//
+// Contains The Bottom Navigation Bar for the app                //
+// Uses a List of Widgets to call the different pages            //
+// Complaints and yelling about this page is for Gaby            //
+//===============================================================//
+
+class BottomBar extends StatefulWidget {
+  @override
+  _BottomBar createState() => _BottomBar();
+}
+
+class _BottomBar extends State<BottomBar> {
+  int _selectedIndex = 0; //int count to select bottom nav bar icon
+
+  List<Widget> _widgetOptions = [
+    HomePage(),
+     // BooksPage(),
+     // MyBooksPage(),
+     // ProfilePage()
+  ];
+
+  //selects bottom nav bar
+  void _onItemTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu_book_rounded),
+            label: 'home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.art_track_rounded),
+            label: 'Books',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'My Books',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTap,
+      ),
+    );
+  }
+}
