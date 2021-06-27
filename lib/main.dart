19a8e89d354616d74c8e80bdb509473552b3051e
jzc0014 <julyncross@my.unt.edu>
2021-06-27 15:48:32 -0500

    Changed initialRoute back to "/WelcomePage" in main.

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: "/MyBooks",
+      initialRoute: "/WelcomePage",
       routes: {
         "/WelcomePage": (context) => WelcomePage(),
         "/ExamplePage": (context) => ExamplePage(),
