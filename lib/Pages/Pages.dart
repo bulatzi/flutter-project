75feea9d99f91112b22d90d53feaf706acaefb8e
SeanBoden <seanboden13@gmail.com>
2021-08-02 22:19:03 -0500

    Removed deleted pages from Pages.dart

--- lib/Pages/Pages.dart
+++ lib/Pages/Pages.dart
@@ -1,6 +1,5 @@
 //If you create a page, include it here.
 
-export 'package:flutter_project/Pages/WelcomePage.dart';
 export 'package:flutter_project/Pages/ExamplePage.dart';
 export 'package:flutter_project/Pages/HomePage.dart';
 export 'package:flutter_project/Pages/BottomBar.dart';
